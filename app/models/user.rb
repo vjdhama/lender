178361e7ef6f1d10ddee82d7084d37780f88e203
Vijay Dhama <vjdhama26@gmail.com>
2014-12-16 00:34:52 +0530

    Added user model user controller index view for user controller

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+	validates_presence_of :name, :email
+	validates_format_of :email , with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, message: "Email is not valid format"
+end
